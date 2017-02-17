50986b20933e2e935bb1b8510229af22d1c387ec
Enoque Joseneas <enoquejoseneas@gmail.com>
2017-02-17 19:54:36 -0300

    adicionado objeto Emile; funcoes loadPlugins e loadAppSettngs movidas para a classe Emile; atualizado conexão entre o tokenListener + main.qml + Emile object

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
-#include <QDir>
 #include <QDebug>
-#include <QJsonArray>
-#include <QQmlContext>
-#include <QJsonObject>
 #include <QTranslator>
-#include <QQuickStyle>
+#include <QQmlContext>
 #include <QQuickWindow>
-#include <QJsonDocument>
 #include <QGuiApplication>
-#include <QRegularExpression>
 #include <QQmlApplicationEngine>
 
 #ifdef Q_OS_ANDROID
@@ -16,61 +10,21 @@
 #include "android/cpp/androidgallery.h"
 #endif
 
+#include "cpp/emile.h"
 #include "cpp/pushnotificationtokenlistener.h"
-
-QJsonArray loadPlugins()
-{
-    QFile pluginsQrc(":/plugins.qrc");
-    QRegularExpression regexp("alias=\"(.*\\.json)?\"");
-    QJsonArray crudArray;
-    QJsonParseError error;
-    if (pluginsQrc.open(QIODevice::ReadOnly)) {
-        QTextStream in(&pluginsQrc);
-        while (!in.atEnd()) {
-            QRegularExpressionMatch match = regexp.match(in.readLine());
-            if (match.hasMatch()) {
-                QFile configJson(":/plugins/" + match.captured(1));
-                if (configJson.open(QIODevice::ReadOnly)) {
-                    QJsonObject jsonObject = QJsonDocument::fromJson(configJson.readAll(), &error).object();
-                    jsonObject["root_folder"] = "/plugins/" + match.captured(1).split('/').first();
-                    crudArray << jsonObject;
-                }
-            }
-        }
-    }
-    pluginsQrc.close();
-    return crudArray;
-}
-
-QVariantMap loadAppConfig()
-{
-    QFile file;
-    file.setFileName(":/settings.json");
-    file.open(QIODevice::ReadOnly | QIODevice::Text);
-    QString settings(file.readAll());
-    file.close();
-    QJsonDocument jsonDocument = QJsonDocument::fromJson(settings.toUtf8());
-    return jsonDocument.object().toVariantMap();
-}
 
 int main(int argc, char *argv[])
 {
-    QVariantMap settings(loadAppConfig());
-
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QCoreApplication::setApplicationName(settings.value("name").toString());
-    QCoreApplication::setOrganizationName(settings.value("name").toString());
-    QCoreApplication::setOrganizationDomain(settings.value("description").toString());
-    QQuickStyle::setStyle("Material");
+    Emile emile;
     QGuiApplication app(argc, argv);
+    emile.setParent(&app);
     Q_INIT_RESOURCE(translations);
-
-    settings.insert("theme", settings.value("theme").toMap().value("material").toMap());
 
     QQmlApplicationEngine engine;
     QQmlContext *context = engine.rootContext();
-    context->setContextProperty("appSettings", settings);
-    context->setContextProperty("crudModel", QVariant::fromValue(loadPlugins()));
+    context->setContextProperty(QLatin1String("Emile"), &emile);
+    context->setContextProperty(QLatin1String("appSettings"), emile.configMap());
+    context->setContextProperty(QLatin1String("crudModel"), QVariant::fromValue(emile.pluginsArray()));
 
     // read for system locale to set in translator object
     QString locale(QLocale::system().name());
@@ -87,18 +41,17 @@ int main(int argc, char *argv[])
 
     #ifdef Q_OS_ANDROID
         AndroidGallery androidgallery;
-        context->setContextProperty("androidGallery", &androidgallery);
+        context->setContextProperty(QLatin1String("androidGallery"), &androidgallery);
     #endif
 
     PushNotificationTokenListener pushNotificationTokenListener;
-    pushNotificationTokenListener.setApplicationSettings(settings);
     pushNotificationTokenListener.setParent(&app);
-    context->setContextProperty("pushNotificationTokenListener", &pushNotificationTokenListener);
 
     engine.load(QUrl(QLatin1String("qrc:/qml/Main.qml")));
 
     QQuickWindow *window = qobject_cast<QQuickWindow *>(engine.rootObjects().value(0));
-    QObject::connect(&pushNotificationTokenListener, SIGNAL(tokenUpdated()), window, SLOT(tokenUpdate()));
+    QObject::connect(&pushNotificationTokenListener, SIGNAL(tokenUpdated(QVariant)), window, SLOT(sendToken(QVariant)));
+    QObject::connect(&pushNotificationTokenListener, SIGNAL(tokenUpdated(QVariant)), &emile, SLOT(registerToken(QVariant)));
 
     return app.exec();
 }
