31e841c3da436e7e5b0e35da892debcc350569bc
Enoque Joseneas <enoquejoseneas@gmail.com>
2017-02-17 19:48:34 -0300

    simplificado o function isValidLoginForm() e adicionado impressao das mensagens em um qsTr()

--- plugins/Session/Functions.js
+++ plugins/Session/Functions.js
@@ -1,31 +1,34 @@
 function isValidLoginForm() {
-    if (email.text === "Email" || email.text.length === 0) {
-        alert("Error!", "Enter your Email!");
-        return false;
-    } else if (!Util.isValidEmail(email.text)) {
-        alert("Error!", "Invalid Email!");
-        return false;
-    } else if (password.text === "Password" || password.text.length === 0) {
+    var message = "";
+
+    if (email.text === "Email" || email.text.length === 0)
+        message = qsTr("Enter your Email!");
+    else if (!Util.isValidEmail(email.text))
+        message = qsTr("Enter your Email!");
+    else if (password.text === "Password" || password.text.length === 0)
+        message = qsTr("Enter your password!");
+
+    if (message.length > 0) {
         alert("Error!", "Enter your password!");
         return false;
     }
+
     return true;
 }
 
 function loginCallback(responseText, status) {
     switch (status) {
     case 404:
-        alert("Error!", "Email or password is invalid. Try again!");
+        alert(qsTr("Error!"), qsTr("Email or password is invalid. Try again!"));
         break;
     case 200:
-        var objectTemp = resultText.user;
+        toast.show(qsTr("Login done successfully!"));
+        var objectTemp = responseText.user;
         window.userProfileData = objectTemp;
-        console.log("window.userProfileData: " + JSON.stringify(window.userProfileData));
-        window.isUserLoggedIn = true;
         loginPopShutdown.start();
         break;
     default:
-        alert(qsTr("Error!"), qsTr("Failed to connect to the server!"))
+        alert(qsTr("Error!"), qsTr("Failed to connect to the server!"));
     }
 }
 
@@ -38,7 +41,6 @@ function requestLogin() {
             "password": password.text
         }
     };
-    jsonListModel.debug = true;
     jsonListModel.requestMethod = "POST";
     jsonListModel.contentType = "application/json";
     jsonListModel.requestParams = JSON.stringify(params);
