a02341faaf671adff3c64589ac8a9a61dca92f6d
Enoque Joseneas <enoquejoseneas@gmail.com>
2017-02-19 02:21:22 -0300

    implementado classe java para permitir minimizar o app sem crash

---
+++ android/src/gsort/pos/engsisubiq/EmileMobile/CustomActivity.java
@@ -0,0 +1,14 @@
+package gsort.pos.engsisubiq.EmileMobile;
+
+public class CustomActivity extends org.qtproject.qt5.android.bindings.QtActivity
+{
+    public CustomActivity()
+    {
+        super();
+    }
+
+    public void minimize()
+    {
+        moveTaskToBack(true);
+    }
+}
