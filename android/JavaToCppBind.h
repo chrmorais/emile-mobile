a47d64bc9342afd3cfb0fcb3813103b6ae3821ce
enoque joseneas <enoquejoseneas@gmail.com>
2017-03-22 15:40:29 -0300

    removido espaçoes em branco e comentários desnecessários

--- android/JavaToCppBind.h
+++ android/JavaToCppBind.h
@@ -9,8 +9,6 @@ static void tokenUpdateNotify(JNIEnv *env, jobject obj, jstring token)
 {
     Q_UNUSED(env)
     Q_UNUSED(obj)
-
-    // Here call send the token to qml function
     PushNotificationTokenListener::tokenUpdateNotify(env->GetStringUTFChars(token,0));
 }
 
@@ -24,18 +22,13 @@ JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved)
     (void)(reserved);
     JNIEnv* env;
     jclass javaClass;
-
-    // if (vm->GetEnv(reinterpret_cast(&env), JNI_VERSION_1_6) < JNI_OK)
     if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK)
         return JNI_ERR;
-
     javaClass = env->FindClass("gsort/pos/engsisubiq/EmileMobile/TokenToApplication");
     if (!javaClass)
         return JNI_ERR;
-
     if (env->RegisterNatives(javaClass, methodsArray, sizeof(methodsArray) / sizeof(methodsArray[0])) < 0)
         return JNI_ERR;
-
     return JNI_VERSION_1_6;
 }
 
