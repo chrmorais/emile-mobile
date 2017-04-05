c4da1c818a6771410ca64d1e5ffca44ec7f34591
Enoque Joseneas <enoquejoseneas@gmail.com>
2017-04-05 16:29:00 -0300

    adicionando passagem dos dados de uma notificação para o app

--- android/src/gsort/pos/engsisubiq/EmileMobile/CustomActivity.java
+++ android/src/gsort/pos/engsisubiq/EmileMobile/CustomActivity.java
@@ -1,5 +1,9 @@
 package gsort.pos.engsisubiq.EmileMobile;
 
+import android.util.Log;
+import android.os.Bundle;
+import android.content.Intent;
+
 public class CustomActivity extends org.qtproject.qt5.android.bindings.QtActivity
 {
     public CustomActivity()
@@ -10,5 +14,19 @@ public class CustomActivity extends org.qtproject.qt5.android.bindings.QtActivit
     public void minimize()
     {
         moveTaskToBack(true);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            String notificationData = bundle.getString("messageData");
+            Log.i("CustomActivity", "onNewIntent(Intent intent)");
+            Log.i("CustomActivity", "notificationData: " + notificationData);
+            // adicionar aqui a passagem dos dados para o app
+            // CustomQtActivity.setPushNotificationArgs(bundle.getString("actionType"), bundle.getString("actionTypeId"));
+        }
     }
 }
