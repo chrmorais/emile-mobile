bfdd75e107cb624a96fe6959a6b607ab4d983417
Enoque Joseneas <enoquejoseneas@gmail.com>
2017-04-11 14:02:15 -0300

    melhoria no envio de notificação pro system tray

--- android/src/gsort/pos/engsisubiq/EmileMobile/FirebaseListenerService.java
+++ android/src/gsort/pos/engsisubiq/EmileMobile/FirebaseListenerService.java
@@ -99,5 +99,7 @@ public class FirebaseListenerService extends FirebaseMessagingService
         }
 
         notificationManager.notify(messageId, notificationBuilder.build());
+        if (!messageData.equals(""))
+            ActivityToApplication.pushNotificationNotify(messageData);
     }
 }
