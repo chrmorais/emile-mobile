7e8419a6bc19ee84a7ba09bc27c7c8fb39786801
Enoque Joseneas <enoquejoseneas@gmail.com>
2017-04-11 17:22:08 -0300

    adicionado status bar transparent

--- android/src/gsort/pos/engsisubiq/EmileMobile/CustomActivity.java
+++ android/src/gsort/pos/engsisubiq/EmileMobile/CustomActivity.java
@@ -1,12 +1,30 @@
 package gsort.pos.engsisubiq.EmileMobile;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.graphics.Color;
 import android.content.Intent;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CustomActivity extends org.qtproject.qt5.android.bindings.QtActivity
 {
     public CustomActivity()
     {
         super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            w.setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 
     @Override
